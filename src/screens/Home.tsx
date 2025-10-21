984118eb7b2eda798b46ddcf7d9e413602c2af49
Andrey Korotkow <andreyrule@gmail.com>
2025-10-22 00:01:38 +0300

    added shift card component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,47 +1,16 @@
-import {
-  Image,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  FlatList,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, FlatList } from 'react-native';
 import { observer } from 'mobx-react-lite';
-import { HomeScreenNavigationProp } from '../navigation/MainNavigator';
 import { shiftsStore } from '../store/shiftsStore';
-import { ShiftsData } from '../types/shifts';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ShiftCard } from '../components/ShiftCard';
 
 export default observer(function HomeScreen() {
-  const navigation = useNavigation<HomeScreenNavigationProp>();
-
-  const renderItem = ({ item }: { item: ShiftsData }) => {
-    return (
-      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
-        <Image
-          source={item.logo ? { uri: item.logo } : undefined}
-          style={styles.logo}
-        />
-        <Text>{item.address}</Text>
-        <Text>{item.companyName}</Text>
-        <Text>{item.dateStartByCity}</Text>
-        <Text>{item.timeStartByCity}</Text>
-        <Text>{item.timeEndByCity}</Text>
-        <Text>{item.currentWorkers}</Text>
-        <Text>{item.planWorkers}</Text>
-        <Text>{item.workTypes[0].name}</Text>
-        <Text>{item.priceWorker}</Text>
-        <Text>{item.customerFeedbacksCount}</Text>
-        <Text>{item.customerRating}</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  console.log(shiftsStore.shifts);
-
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList data={shiftsStore.shifts} renderItem={renderItem} />
+      <FlatList
+        data={shiftsStore.shifts}
+        renderItem={(item) => <ShiftCard item={item.item} />}
+      />
     </SafeAreaView>
   );
 });
@@ -51,9 +20,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  logo: {
-    width: 100,
-    height: 100,
   },
 });
